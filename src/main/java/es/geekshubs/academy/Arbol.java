a8a94b71cc7f1bc6ca76ab466687308549741e49
DavidVH <60264680+dvaqueroh@users.noreply.github.com>
2021-08-17 13:48:35 +0200

    implemento el metodo apply
    dejo las impresiones por consola para comprobacion

--- src/main/java/es/geekshubs/academy/Arbol.java
+++ src/main/java/es/geekshubs/academy/Arbol.java
@@ -1,6 +1,7 @@
 package es.geekshubs.academy;
 
 import java.util.Collections;
+import java.util.Iterator;
 
 public class Arbol {
 
@@ -9,8 +10,37 @@ public class Arbol {
    * @param input
    * @return
    */
-  public String apply(final Integer input) {
-      return null;
-  }
+	public String apply(final Integer input) {
+		  String result = "";
+		  System.out.println("lado: "+input);
+		  
 
-}
+		for (int i = 1;i <= input;i++) {
+			result += " ";
+			for(int j = 1;j <=input-i;j++) {
+				result += " ";
+			}
+			for(int j = 1;j < i;j++) {
+				result += "*";
+			}
+			result += "\n";
+			
+		}// fin for
+		  
+		  result += lado(input); // crea la ultima fila desde el metodo
+		  System.out.println(result);
+		  
+	      return result;
+    }// fin aplly
+	
+	public String lado(int input) { // metodo crea una fila con el numero de "*" dado
+		String lado="";
+		for (int i = 0; i < input; i++) { //FILA
+			
+			lado += "*";
+		}
+		
+		return lado;
+	}
+
+}// fin clase
